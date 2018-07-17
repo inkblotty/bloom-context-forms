b8667a56be0895f76d30988cababc8aa6f01d0e0
Katie Foster <fosterkatiea@gmail.com>
2018-07-17 15:02:19 -0600

    tweaking eslint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import FormHandler from './form-handler';
-import { connectForm, FormContext, getCurrentContext } from './form-context';
-import { validatorAggregator as validator } from './validator';
+import FormHandler from './form-handler'
+import { connectForm, FormContext, getCurrentContext } from './form-context'
+import { validatorAggregator as validator } from './validator'
 
-export { connectForm, FormContext, FormHandler, getCurrentContext, validator };
+export { connectForm, FormContext, FormHandler, getCurrentContext, validator }
